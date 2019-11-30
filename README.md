029af4c82af8605f045439a85555372ddb88c32c
Erwan Lemonnier <erwan@lemonnier.se>
2019-11-30 23:55:56 +0100

    Use README as pypi description

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # pymacaron-gcp
 
 A deployment pipeline for pymacaron microservices, deploying them as Docker
-containers on Google Cloud Run.
+containers on Google Kubernetes Engine (GKE) or Google Cloud Run (GCR).
 
 See more at [pymacaron.com](http://pymacaron.com/gcp.html)
+
+## Author
+
+Erwan Lemonnier<br/>
+[github.com/pymacaron](https://github.com/pymacaron)</br>
+[github.com/erwan-lemonnier](https://github.com/erwan-lemonnier)<br/>
+[www.linkedin.com/in/erwan-lemonnier/](https://www.linkedin.com/in/erwan-lemonnier/)
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from os import path
 import sys
 import os
 from glob import glob
@@ -30,6 +31,11 @@ if version:
     version = version.strip()
 print("version: %s" % version)
 
+# read the contents of the README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='pymacaron-gcp',
     version=version,
@@ -37,7 +43,9 @@ setup(
     license='BSD',
     author='Erwan Lemonnier',
     author_email='erwan@lemonnier.se',
-    description='Deploy a PyMacaron microservice to Google Cloud Run',
+    description='PyMacaron deployment pipelines towards Google Kubernetes Engine (GKE) and Cloud Run (GCR)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
     ],
     tests_require=[
